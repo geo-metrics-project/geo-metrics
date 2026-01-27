41251303cd48de2334accfd08c32dae19a6424db
SimbaDuZOO <antoinecombaldieu@gmail.com>
2026-01-27 01:23:17 +0100

    fixed env variable not present at build

--- frontend/src/lib/kratos.ts
+++ frontend/src/lib/kratos.ts
@@ -2,7 +2,7 @@ import { Configuration, FrontendApi } from "@ory/client"
 
 export const kratosClient = new FrontendApi(
   new Configuration({ 
-    basePath: process.env.NEXT_PUBLIC_KRATOS_URL || "http://kratos.combaldieu.fr",
+    basePath: process.env.NEXT_PUBLIC_KRATOS_URL || "https://kratos.combaldieu.fr",
     baseOptions: { 
       withCredentials: true 
     }
