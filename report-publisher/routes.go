1faba87520b82545ec05ee9bde98f301b3869521
AntoineCmbld <antoinecombaldieu@gmail.com>
2026-03-22 19:33:00 +0100

    changed route

--- report-publisher/routes.go
+++ report-publisher/routes.go
@@ -4,5 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func registerRoutes(r *gin.Engine, publisher *natsPublisher) {
 	r.GET("/api/health", healthHandler)
-	r.POST("/api/reports/new", createReportHandler(publisher))
+	r.POST("/api/reports", createReportHandler(publisher))
 }
